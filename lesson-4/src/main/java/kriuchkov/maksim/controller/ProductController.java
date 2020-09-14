a3dbf7005882b0b92533ed4b971e497fa3b4df14
observant-sun <7293121+observant-sun@users.noreply.github.com>
2020-09-14 09:14:29 +0300

    Tasks 1, 2

---
+++ lesson-4/src/main/java/kriuchkov/maksim/controller/ProductController.java
@@ -0,0 +1,56 @@
+package kriuchkov.maksim.controller;
+
+import kriuchkov.maksim.persist.entity.Product;
+import kriuchkov.maksim.persist.repo.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public String getProducts(Model model) {
+        List<Product> list = repository.findAll();
+        model.addAttribute("products", list);
+        return "products";
+    }
+
+    @GetMapping("/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product(-1, "", null));
+        return "product";
+    }
+
+    @GetMapping("/{id}")
+    public String getProduct(@PathVariable("id") Integer id, Model model) {
+        Optional<Product> productOptional = repository.findById(id);
+        if (productOptional.isPresent()) {
+            model.addAttribute("product", productOptional.get());
+            return "product";
+        } else {
+            return "product"; // TODO: вывести HTTP 404
+        }
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(Product product) {
+        if (repository.findById(product.getId()).isPresent())
+            repository.save(product);
+        else
+            repository.save(product);
+        return "redirect:/product";
+    }
+
+}
---
+++ lesson-4/src/main/java/kriuchkov/maksim/persist/repo/ProductRepository.java
@@ -0,0 +1,12 @@
+package kriuchkov.maksim.persist.repo;
+
+import kriuchkov.maksim.persist.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
