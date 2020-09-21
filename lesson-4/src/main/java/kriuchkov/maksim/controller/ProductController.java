f82577f476effe29e023e9897d2e960913a27c12
observant-sun <7293121+observant-sun@users.noreply.github.com>
2020-09-21 14:03:08 +0300

    Filtering using Criteria API and Spring Data Specifications

--- lesson-4/src/main/java/kriuchkov/maksim/controller/ProductController.java
+++ lesson-4/src/main/java/kriuchkov/maksim/controller/ProductController.java
@@ -2,12 +2,19 @@ package kriuchkov.maksim.controller;
 
 import kriuchkov.maksim.persist.entity.Product;
 import kriuchkov.maksim.persist.repo.ProductRepository;
+import kriuchkov.maksim.persist.repo.UserSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.criteria.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,20 +25,45 @@ public class ProductController {
     @Autowired
     private ProductRepository repository;
 
+//  для варианта с CriteriaBuilder
+
+//    @Autowired
+//    private EntityManagerFactory entityManagerFactory;
+//
+//    private EntityManager em;
+//
+//    @PostConstruct
+//    public void init() {
+//        em = entityManagerFactory.createEntityManager();
+//    }
+
     @GetMapping
     public String getProducts(Model model,
                               @RequestParam(value = "priceMin", required = false) BigDecimal priceMin,
                               @RequestParam(value = "priceMax", required = false) BigDecimal priceMax) {
-        List<Product> list;
 
-        if (priceMin == null && priceMax == null)
-            list = repository.findAll();
-        else if (priceMin != null && priceMax == null)
-            list = repository.findByPriceGreaterThanEqual(priceMin);
-        else if (priceMin == null)
-            list = repository.findByPriceLessThanEqual(priceMax);
-        else
-            list = repository.findByPriceGreaterThanEqualAndPriceLessThanEqual(priceMin, priceMax);
+
+//  вариант с CriteriaBuilder
+
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Product> query = cb.createQuery(Product.class);
+//        Root<Product> from = query.from(Product.class);
+//
+//        List<Predicate> predicates = new ArrayList<>();
+//        if (priceMin != null)
+//            predicates.add(cb.ge(from.get("price"), priceMin));
+//        if (priceMax != null)
+//            predicates.add(cb.le(from.get("price"), priceMax));
+//
+//        CriteriaQuery<Product> cq = query.select(from).where(predicates.toArray(new Predicate[0]));
+//        List<Product> list = em.createQuery(cq).getResultList();
+
+        Specification<Product> spec = UserSpecification.trueLiteral();
+        if (priceMin != null)
+            spec = spec.and(UserSpecification.priceGreaterOrEqual(priceMin));
+        if (priceMax != null)
+            spec = spec.and(UserSpecification.priceLessOrEqual(priceMax));
+        List<Product> list = repository.findAll(spec);
 
         model.addAttribute("products", list);
         return "products";
@@ -58,8 +90,7 @@ public class ProductController {
     public String updateProduct(Product product) {
         if (repository.findById(product.getId()).isPresent())
             repository.save(product);
-        else
-            repository.save(product);
+        // TODO: else error?
         return "redirect:/product";
     }
 
--- lesson-4/src/main/java/kriuchkov/maksim/persist/repo/ProductRepository.java
+++ lesson-4/src/main/java/kriuchkov/maksim/persist/repo/ProductRepository.java
@@ -2,16 +2,10 @@ package kriuchkov.maksim.persist.repo;
 
 import kriuchkov.maksim.persist.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findByPriceGreaterThanEqual(BigDecimal minPrice);
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
 
-    List<Product> findByPriceLessThanEqual(BigDecimal maxPrice);
-
-    List<Product> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal minPrice, BigDecimal maxPrice);
 }
